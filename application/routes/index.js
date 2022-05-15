23ac8e7b81a35db0118405b83edef1f6d76de100
ArjunS132 <arjunsgill132@gmail.com>
2022-05-14 23:33:47 -0700

    set up middleware so that posts can only be made by logged in users

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var isLoggedIn = require('../middleware/routeprotectors').userIsLoggedIn;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('home');
+});
+
+router.get('/home', function(req, res, next) {
   res.render('home');
 });
 
@@ -14,16 +19,14 @@ router.get('/registration', function(req, res, next) {
   res.render('registration');
 });
 
+
 router.get('/index', function(req, res, next) {
-  res.render('index');
+  res.render('home');
 });
 
+router.use('/postimage', isLoggedIn);
 router.get('/postimage', function(req, res, next) {
   res.render('postimage');
-});
-
-router.get('/home', function(req, res, next) {
-  res.render('home');
 });
 
 router.get('/viewpost', function(req, res, next) {
