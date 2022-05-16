12ee95c61e507da654c6ffd267656dac0e4daa22
ArjunS132 <arjunsgill132@gmail.com>
2022-05-15 20:23:06 -0700

    made viewpost look nicer

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var isLoggedIn = require('../middleware/routeprotectors').userIsLoggedIn;
 var getRecentPosts = require('../middleware/postmiddleware').getRecentPosts;
+var db = require('../config/database');
 
 /* GET home page. */
 router.get('/', getRecentPosts, function(req, res, next) {
@@ -32,6 +33,27 @@ router.get('/postimage', function(req, res, next) {
 
 router.get('/viewpost', function(req, res, next) {
   res.render('viewpost');
+});
+
+router.get('/post/:id(\\d+)', (req,res,next) => {
+    let baseSQL =  "SELECT u.username, p.title, p.description, p.photopath, p.createdAt \
+                    FROM users u \
+                    JOIN posts p \
+                    ON u.id=fk_userId \
+                    WHERE p.id=?;"
+
+    let postId = req.params.id;
+
+    db.execute(baseSQL, [postId])
+    .then(([ results, fields ]) => {
+        if(results && results.length){
+            let post = results[0]
+            res.render('viewpost', {currentPost: post});
+        } else{
+            req.flash('error', 'This is not the post you are looking for!')
+            res.redirect('/');
+        }
+    });
 });
 
 module.exports = router;
