d8a3f0e2db290e7ac9f29d32f11f98a79c1a5d76
ArjunS132 <arjunsgill132@gmail.com>
2022-05-16 19:36:01 -0700

    set up the front end for searching for posts

--- application/public/js/homeJs.js
+++ application/public/js/homeJs.js
@@ -1,76 +1,71 @@
-
-// function fadeOut(theDiv){
-//     let mainDiv = document.getElementById('viewpost-container');
-//     var interval = setInterval(changeOpacity, 20);
-//     function changeOpacity() {
-//         let opacity = theDiv.style.opacity;
-//         if( opacity > 0 ) {
-//             opacity -= 0.1;
-//             theDiv.style.opacity = opacity;
-//         }
-//         if( opacity == 0 )
-//         {
-//             mainDiv.removeChild(theDiv);
-//             clearInterval(interval);
-//             regex = /\d/g;
-//             theEle = document.getElementById('numItems');
-//             numItems = theEle.innerHTML;
-//             numElements = numItems.match(regex);
-//             numElements = numElements.join("");
-//             console.log(numElements);
-//             numElements -= 1;
-//             console.log(numElements);
-//             theEle.innerHTML = `There are ${numElements} photo(s) being shown`;
-//         }
-//     }
-// }
-//
-// function createPhotoCard(data, containerDiv) {
-//     let miniGrid = document.createElement("div");
-//     miniGrid.className = "miniGrid"
-//     miniGrid.id = data["id"];
-//     miniGrid.style.opacity = 1;
-//     miniGrid.setAttribute( "onClick", "fadeOut(this)");
-//     let image = document.createElement("img");
-//     image.src = data["url"];
-//     let title = document.createElement("h1");
-//     title.innerHTML = data["title"];
-//     miniGrid.appendChild(image);
-//     miniGrid.appendChild(title);
-//     containerDiv.appendChild(miniGrid);
-// }
-//
-// let mainDiv = document.getElementById('viewpost-container');
-//
-// if(mainDiv) {
-//     let fetchUrl = "https://jsonplaceholder.typicode.com/albums/2/photos"
-//     fetch(fetchUrl)
-//     .then((data) => data.json() )
-//     .then((photos) => {
-//         let innerHtml = "";
-//         photos.forEach((photo) => {
-//             createPhotoCard(photo, mainDiv);
-//         });
-//         numItems = document.getElementById('numItems');
-//         numItems.innerHTML = `There are ${photos.length} photo(s) being shown`;
-//     })
-// }
-
-function setFlashMessageFadeOut() {
+function setFlashMessageFadeOut(flashMessageElement) {
     setTimeout(() => {
         let currentOpacity = 1.0;
         let timer = setInterval(() => {
             if(currentOpacity < 0.05) {
                 clearInterval(timer);
-                flashElement.remove();
+                flashMessageElement.remove();
             }
             currentOpacity = currentOpacity - .05;
-            flashElement.style.opacity = currentOpacity;
+            flashMessageElement.style.opacity = currentOpacity;
         }, 50);
     },4000);
 }
 
+function addFlashFromFrontEnd(message) {
+    let flashMessageDiv = document.createElement('div');
+    let innerFlashDiv = document.createElement('div');
+    let innerTextNode = document.createTextNode(message);
+    innerFlashDiv.appendChild(innerTextNode);
+    flashMessageDiv.appendChild(innerFlashDiv);
+    flashMessageDiv.setAttribute('id', 'flash-message');
+    innerFlashDiv.setAttribute('class', 'alert alert-info');
+    document.getElementsByTagName('body')[0].appendChild(flashMessageDiv);
+    setFlashMessageFadeOut(flashMessageDiv);
+}
+
+function createCard(postData) {
+    return `<div id="post-${postData.id}" class="card">
+        <img class="card-image" src="${postData.thumbnailpath}" alt="Missing Image">
+        <div class="card-body">
+            <p class="card-title">${postData.title}</p>
+            <p class="card-description">${postData.description}</p>
+            <a href="/post/${postData.id}" class="looksLikeButton">Post Details</a>
+        </div>
+    </div>`;
+}
+
+function executeSearch() {
+    let searchTerm = document.getElementById('search-text').value;
+    if(!searchTerm) {
+        location.replace('/');
+        return;
+    }
+    let mainContent = document.getElementById('home-container');
+    let searchURL = `/posts/search?search=${searchTerm}`
+    fetch(searchURL)
+    .then( (data) => {
+        return data.json();
+    })
+    .then((data_json) => {
+        let newMainContentHTML = '';
+        data_json.results.forEach((row) => {
+            newMainContentHTML += createCard(row);
+        });
+        mainContent.innerHTML = newMainContentHTML;
+        if(data_json.message) {
+            addFlashFromFrontEnd(data_json.message);
+        }
+    })
+    .catch( (err) => console.log(err));
+}
+
 let flashElement = document.getElementById('flash-message');
 if(flashElement){
-    setFlashMessageFadeOut();
+    setFlashMessageFadeOut(flashElement);
+}
+
+let searchButton = document.getElementById('search-button');
+if(searchButton){
+    searchButton.onclick = executeSearch;
 }
